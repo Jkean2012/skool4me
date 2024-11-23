2ea6a12c9a1ae83ad2131b3eed325bbaf6066407
Jkean2012 <Jkean2012@outlook.com>
2024-11-23 15:24:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function openGame() {
-    window.open("about:blank", "_blank");
+    // Replace with the actual URL you want to open
+    const gameUrl = "https://jkean2012.github.io/skool4me";  // Put the URL you want to "cloak" here
+    window.open(gameUrl, "_blank");
 }
